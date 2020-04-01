e3c85a140055d91bb0a295d81320a60451df1573
JoseMobile <dair3400@mylaurier.ca>
2020-04-01 12:12:20 -0400

    Add files via upload

--- OneStepSampleSigma.R
+++ OneStepSampleSigma.R
@@ -3,10 +3,10 @@ require("mniw")
 #' Samples a list of matrices denoting the covariance matrices for each cluster
 #' 
 #' @param A 'n x q' matrix where each row is an observation and the columns are a subset of a larger parameter set.
-#' @param mu An list of K 'p x 1' vectors denoting the column means of a cluster.
+#' @param mu An array of K 'p x 1' vectors denoting the column means of a cluster.
 #' @param N A 'K x 1' vector denoting the counts in each cluster
 #' @param K A scalar denoting the number of clusters specified
-#' @return An 'K x 1' list of matrices sampled from the inverse-wishart distribution where the kth matrix
+#' @return An array of matrices sampled from the inverse-wishart distribution where the kth matrix
 #' denotes the posterior covariance matrix of the kth cluster   
 #' @details The list of matrices that is returned is  `Sigma_k | A \ \{theta_k\}` which had been sampled from
 #' ```
@@ -18,10 +18,12 @@ require("mniw")
 OneStepSampleSigma <- function(theta, mu,N,K){
   n <- length(theta)
   p <- rep(ncol(theta[1],K)) # vector of scalars where each scalar is the size of any theta_i
-  theta_mu <- theta - mu # get scale parameter
+  theta_mu <- theta - mu 
   # get vector of degrees of freedom
   nu <- N - p - 1
-  Psi <- theta_mu %*% theta_mu
+  Psi <- theta_mu %*% theta_mu # get scale parameter
   riwish(K,Psi = Psi,nu = nu)
 }
+
+
 
--- OneStepSampleTheta.R
+++ OneStepSampleTheta.R
@@ -5,11 +5,11 @@ require("mniw")
 #' Samples a matrix, theta_i | A \ {theta_i}, from distribution N(G_i %% (y_i - mu_zi) - mu_zi,G_i %% V_i)
 #' where G_i = Sigma_zi %% (V_i - Sigma_zi)^(-1), zi is a randomly sampled index term
 #' 
-#' @param Y An 'N x 1' list of 'n x 1' vectors denoting the features of a single observation.
-#' @param V An 'N x 1' list of Observed fisher information matrices
-#' @param mu An 'N x 1' list of n x 1' vectors denoting the prior means.
-#' @param Sigma An 'N x 1' vector of 'n x n' matrices denoting the prior variances for each cluster.
-#' @return An N 'n x 1' vectors that is sampled from mixture normal random effects model.
+#' @param Y An matrix denoting the features of a single observation.
+#' @param V An array of Observed fisher information matrices
+#' @param mu A matrix denoting the prior column means of each cluster.
+#' @param Sigma An array of 'n x n' matrices denoting the prior variances for each cluster.
+#' @return An array 'n x 1' vectors that is sampled from mixture normal random effects model.
 #' @details The vector that is returned is  `theta_i | A \ \{theta_i\}` which had been sampled from
 #' ```
 #' `N(G_i(y_i - mu_z_i) - mu_z_i,G_i V_i)``
@@ -17,8 +17,7 @@ require("mniw")
 #' ```
 
 OneStepSampleTheta <- function(Y,V,mu,Sigma){
-  N <- length(Y)
-  theta <- rRxNorm(N,Y,V,mu, Sigma)
+  theta <- rRxNorm(1,Y,V,mu, Sigma)
   theta
 }
   
